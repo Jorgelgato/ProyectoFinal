a4c22f547c13fbf720318ac6c4565f84ab41a8ae
Jorge Bobrek <jrbobrek@gmail.com>
2022-01-03 13:02:13 -0500

    FIX - Client login
    - Fixed salt for password hash

--- proyecto-final-frontend/src/app/services/encrypt.service.ts
+++ proyecto-final-frontend/src/app/services/encrypt.service.ts
@@ -9,11 +9,12 @@ export class EncryptService {
   constructor() { }
 
   encrypt(password: string) {
-    const salt = bcrypt.genSaltSync(10);
+    const salt = "$2a$10$LRzl4OGEb4Xaaot9sMSslu";
     return bcrypt.hashSync(password, salt);
   }
 
   compare(password: string, password2: string) {
     return bcrypt.compareSync(password, password2);
   }
+
 }
